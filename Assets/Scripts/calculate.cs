481411eaa37537050c5c02e07c4d3e33f01fe24b
jackharrop <jackharrop@education.nsw.gov.au>
2021-04-30 09:49:17 +1000

    created Ui and made a new script for button

---
+++ Assets/Scripts/calculate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class calculate : MonoBehaviour
+{
+    public InputField GrossSalaryInput;
+    public Dropdown SalaryPayPeriod;
+    public Text MedicareOutput;
+    public Text NetIncomeOutput;
+    public Text IncomeTaxPaidOutput;
+
+    public void ButtonClicked()
+    {
+
+
+    }
+}
