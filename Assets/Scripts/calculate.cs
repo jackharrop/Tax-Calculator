72d6b3d9e0f8466a335f5e7dd02bc1991a13fe7e
jackharrop <jackharrop@education.nsw.gov.au>
2021-05-04 12:42:48 +1000

    yes

--- Assets/Scripts/calculate.cs
+++ Assets/Scripts/calculate.cs
@@ -11,9 +11,54 @@ public class calculate : MonoBehaviour
     public Text NetIncomeOutput;
     public Text IncomeTaxPaidOutput;
 
-    public void ButtonClicked()
-    {
 
+    public void claculate()
+    {
+        
+    }
+
+    private double ClaculateIncomeTax(double GrossYearlySalary)
+    {
+        if (GrossYearlySalary > 0 && GrossYearlySalary < 18200)
+        {
+            return 0;
+        }
+        else if (GrossYearlySalary < 37000)
+        {
+            return 0.19 * (GrossYearlySalary - 18200);
+        }
+        else if (GrossYearlySalary < 87000)
+        {
+            return 0.19 * (GrossYearlySalary - 18200);
+        }
+    }
+    private double GrossYearlySalary()
+    {
+        if (double.TryParse(GrossSalaryInput.text, out double GetGrossSalary))
+        {
+            if (SalaryPayPeriod.value == 0)
+            {
+                return GetGrossSalary * 52;
+            }
+            else if (SalaryPayPeriod.value == 1)
+            {
+                return GetGrossSalary * 26;
+            }
+            else if (SalaryPayPeriod.value == 3)
+            {
+                return GetGrossSalary * 12;
+            }
+            else if (SalaryPayPeriod.value == 4)
+            {
+                return GetGrossSalary * 1;
+            }
+
+
+
+        }
 
     }
+    
+   
 }
+
