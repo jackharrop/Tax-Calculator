3b4b547f3d98e1489604f82b37ded9338defad58
jackharrop <jackharrop@education.nsw.gov.au>
2021-05-06 12:37:01 +1000

    very good finished

--- Assets/Scripts/calculate.cs
+++ Assets/Scripts/calculate.cs
@@ -6,59 +6,123 @@ using UnityEngine.UI;
 public class calculate : MonoBehaviour
 {
     public InputField GrossSalaryInput;
-    public Dropdown SalaryPayPeriod;
+    public Dropdown SalaryPayPeriodDropdown;
     public Text MedicareOutput;
     public Text NetIncomeOutput;
     public Text IncomeTaxPaidOutput;
+    public Text GrossYearlySalaryOutput;
 
 
-    public void claculate()
+    public void calculates()
     {
         
+         
+        double grossSalaryInput = GetGrossSalary();
+        string SalaryPayPeriod = GetSalaryPayPeriod();
+        
+        double MedicareLevyPaid = CalculateMedicareLevy(grossSalaryInput);
+
+        double grossYearlySalary = CalculateGrossYearlySalary( grossSalaryInput, SalaryPayPeriod);
+        
+        double IncomeTaxPaid = CalculateIncomeTax(grossYearlySalary);
+
+        double NetIncome = CalculateNetIncome( grossYearlySalary, ref MedicareLevyPaid, ref IncomeTaxPaid);
+        
+
+        OutputResults(MedicareLevyPaid, IncomeTaxPaid, NetIncome, grossYearlySalary);
+        //output
     }
 
-    private double ClaculateIncomeTax(double GrossYearlySalary)
+   
+    private double GetGrossSalary()
     {
-        if (GrossYearlySalary > 0 && GrossYearlySalary < 18200)
+        double GrossSalary = double.Parse(GrossSalaryInput.text);
+        return GrossSalary;
+    }
+    private string GetSalaryPayPeriod()
+    {
+        if (SalaryPayPeriodDropdown.value == 0)
+        {
+            return "Weekly";
+        }
+        else if (SalaryPayPeriodDropdown.value == 1)
+        {
+            return "Fortnightly";
+        }
+        else if (SalaryPayPeriodDropdown.value == 0)
+        {
+            return "Monthly";
+        }
+        else 
+        {
+            return "Yearly";
+        }
+    }
+
+    private double CalculateGrossYearlySalary(double grossSalaryInput, string SalaryPayPeriod)
+    {
+        if(SalaryPayPeriod == "Weekly")
+        {
+            return grossSalaryInput * 52;
+        }
+        else if (SalaryPayPeriod == "Fortnightly")
+        {
+            return grossSalaryInput * 26;
+        }
+        else if (SalaryPayPeriod == "Monthly")
+        {
+            return grossSalaryInput * 12;
+        }
+        else
+        {
+            return grossSalaryInput * 1;
+        }
+    }
+
+    private double CalculateIncomeTax(double grossYearlySalary)
+    {
+        if (grossYearlySalary > 0 && grossYearlySalary < 18200)
         {
             return 0;
         }
-        else if (GrossYearlySalary < 37000)
+        else if (grossYearlySalary < 37000)
         {
-            return 0.19 * (GrossYearlySalary - 18200);
+            return 0.19 * (grossYearlySalary - 18200);
         }
-        else if (GrossYearlySalary < 87000)
+        else if (grossYearlySalary < 87000)
         {
-            return 0.19 * (GrossYearlySalary - 18200);
+            return 3573 + 0.325 * (grossYearlySalary - 37000);
+        }
+        else if (grossYearlySalary < 180000)
+        {
+            return 19822 + 0.37 * (grossYearlySalary - 87000);
+        }
+        else if (grossYearlySalary > 180000)
+        {
+            return 54232 + 0.45 * (grossYearlySalary - 18000);
+        }
+        else
+        {
+            return 0;
         }
     }
-    private double GrossYearlySalary()
+    private double CalculateMedicareLevy(double grossYearlySalary)
     {
-        if (double.TryParse(GrossSalaryInput.text, out double GetGrossSalary))
-        {
-            if (SalaryPayPeriod.value == 0)
-            {
-                return GetGrossSalary * 52;
-            }
-            else if (SalaryPayPeriod.value == 1)
-            {
-                return GetGrossSalary * 26;
-            }
-            else if (SalaryPayPeriod.value == 3)
-            {
-                return GetGrossSalary * 12;
-            }
-            else if (SalaryPayPeriod.value == 4)
-            {
-                return GetGrossSalary * 1;
-            }
-
-
-
-        }
+        return grossYearlySalary * 0.02;
+    }
+    private double CalculateNetIncome(double grossYearlySalary , ref double MedicareLevyPaid, ref double IncomeTaxPaid)
+    {
+        return grossYearlySalary - MedicareLevyPaid - IncomeTaxPaid;
+    }
+    private void OutputResults(double MedicareLevyPaid, double IncomeTaxPaid, double NetIncome, double grossYearlySalary)
+    {
+        MedicareOutput.text = "Medicare levy paid: $" + MedicareLevyPaid.ToString("F2");
+        NetIncomeOutput.text = "Net income paid: $" + NetIncome.ToString("F2");
+        GrossYearlySalaryOutput.text = "Gross salary paid: $" + grossYearlySalary.ToString("F2");
+        IncomeTaxPaidOutput.text = "Income tax paid: $" + IncomeTaxPaid.ToString("F2");
 
     }
-    
-   
+
+
 }
 
